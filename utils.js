86b94d41aa4370e1bf50b8f1455b99138f2508d5
Bianca Zanardi <bianca.zanardi@gmail.com>
2021-12-07 21:55:25 +0100

    Add range, max, min, sum, sumby to utils

--- utils.js
+++ utils.js
@@ -9,3 +9,50 @@ const loadDay = (day) => {
 exports.datefy = (day, mapper = identity, separator = '\n') => {
   return loadDay(day).split(separator).map(mapper);
 };
+
+/** @function
+ * Creates an array of numbers progressing from start up to, but not including, end.
+ * Accepts an optional step, otherwise 1 is used.
+ */
+exports.range = (from, to, step = 1) => {
+  const arr = [];
+  for (i = from; i < to; i+= step) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+/** @method
+ * Computes the maximum value of array. If array is empty, undefined is returned.
+ * Accepts an optional defaultMax.
+ */
+ function max(defaultMax) {
+  return this.reduce((x, b) => Math.max(x, b)) ?? defaultMax;
+}
+
+/** @method
+ * Computes the minimum value of array. If array is empty, undefined is returned.
+ * Accepts an optional defaultMin.
+ */
+ function min(defaultMin) {
+  return this.reduce((x, y) => Math.min(x, y)) ?? defaultMin;
+}
+
+/** @method
+ * Computes the sum of the values in array.
+ */
+ function sum(defaultVal = 0) {
+  return this.reduce((x, y) => x + y, defaultVal);
+}
+
+/** @method
+ * Computes the sum of the values in array while processing each of them with an iteratee.
+ */
+function sumBy(fn, defaultVal = 0) {
+  return this.map(fn).reduce((x, y) => x + y, defaultVal);
+}
+
+Array.prototype.max = max;
+Array.prototype.min = min;
+Array.prototype.sum = sum;
+Array.prototype.sumBy = sumBy;
